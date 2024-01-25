bbad5f4a2fee9b52ca8b3ce8f6df614b74a15cbf
devmianharoon <devmianharoon@gmail.com>
2024-01-25 16:53:16 +0500

    Dynammic Blog Using Contentful CMS

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ['images.ctfassets.net'],
-      },
+  images: {
+      domains: ['images.ctfassets.net'],
+  },
+  experimental: {
+      trailingSlash: false,
+  },
 };
-module.exports = nextConfig
+
+module.exports = nextConfig;
